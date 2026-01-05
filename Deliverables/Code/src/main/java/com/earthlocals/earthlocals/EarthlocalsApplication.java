8683baad26956ab2b825902615c68e7c88ff4fe8
Andrea Squitieri <andrea.squitieri@disroot.org>
2026-01-05 10:46:29 +0100

    feat: initialized project code

---
+++ Deliverables/Code/src/main/java/com/earthlocals/earthlocals/EarthlocalsApplication.java
@@ -0,0 +1,13 @@
+package com.earthlocals.earthlocals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthlocalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthlocalsApplication.class, args);
+	}
+
+}
---
+++ Deliverables/Code/src/test/java/com/earthlocals/earthlocals/EarthlocalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthlocals.earthlocals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthlocalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
