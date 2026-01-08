90c705643d82380e9f39a9a71c8a78db41cb9972
staarsnap <m.rega26@studenti.unisa.it>
2026-01-08 11:06:17 +0100

    feat: created initial page layout and drafted home page

---
+++ Deliverables/Code/src/main/java/com/earthlocals/earthlocals/system/HomeController.java
@@ -0,0 +1,16 @@
+package com.earthlocals.earthlocals.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
+
+}
