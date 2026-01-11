04df935c6e831a3325f8a087639c7b05dc4880d7
staarsnap <m.rega26@studenti.unisa.it>
2026-01-11 18:12:08 +0100

    feat: added functionality to GestioneCandidatura

--- Deliverables/Code/src/main/java/com/earthlocals/earthlocals/model/CandidaturaRepository.java
+++ Deliverables/Code/src/main/java/com/earthlocals/earthlocals/model/CandidaturaRepository.java
@@ -2,12 +2,30 @@ package com.earthlocals.earthlocals.model;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface CandidaturaRepository extends CrudRepository<Candidatura, Long> {
     Page<Candidatura> findByCandidato(Volontario volontario, Pageable pageable);
 
     Page<Candidatura> findByStatoAndCandidato(Candidatura.CandidaturaStato candidaturaStato, Utente utente, Pageable pageable);
 
+    @Query("SELECT c FROM Candidatura c JOIN c.missione m WHERE m.creatore = ?1")
+    Page<Candidatura> findByOrganizzatore(Utente utente, Pageable pageable);
+
+    @Query("SELECT c FROM Candidatura c JOIN c.missione m WHERE c.candidato = :candidato AND c.stato = :stato AND m.internalStato = :missioneStato AND m.dataFine <= :data")
+    Page<Candidatura> findByCandidatoAndStatoAndMissioneInternalStatoAndMissioneDataFineBefore(
+            @Param("candidato") Utente candidato,
+            @Param("stato") Candidatura.CandidaturaStato stato,
+            @Param("missioneStato") Missione.InternalMissioneStato missioneStato,
+            @Param("data") LocalDate data,
+            Pageable pageable);
+    
     boolean existsByMissioneAndCandidato(Missione missione, Volontario candidato);
+
+    Candidatura findByMissioneAndCandidato(Missione missione, Volontario volontario);
+
 }
--- Deliverables/Code/src/main/java/com/earthlocals/earthlocals/service/gestionecandidature/GestioneCandidatura.java
+++ Deliverables/Code/src/main/java/com/earthlocals/earthlocals/service/gestionecandidature/GestioneCandidatura.java
@@ -6,9 +6,15 @@ import com.earthlocals.earthlocals.service.gestionecandidature.exceptions.Candid
 import com.earthlocals.earthlocals.service.gestionecandidature.exceptions.CandidaturaNotAcceptableException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -59,4 +65,72 @@ public class GestioneCandidatura {
     private boolean hasVolontarioAlreadyApplied(Missione missione, Volontario candidato) {
         return candidaturaRepository.existsByMissioneAndCandidato(missione, candidato);
     }
+
+    public void removeCandidatura(CandidaturaDTO candidaturaDTO) {
+        Missione missione = missioneRepository
+                .findById(candidaturaDTO.getMissioneId())
+                .orElseThrow(() -> new IllegalArgumentException("Missione non trovata"));
+        //Idem
+        Volontario candidato = volontarioRepository
+                .findById(candidaturaDTO.getCandidatoId())
+                .orElseThrow(() -> new IllegalArgumentException("Utente non trovato"));
+        if (!hasVolontarioAlreadyApplied(missione, candidato)) {
+            return;
+        }
+        Candidatura candidatura = candidaturaRepository.findByMissioneAndCandidato(missione, candidato);
+        candidaturaRepository.delete(candidatura);
+    }
+
+    public boolean acceptCandidatura(Long id) {
+        var candidatura = candidaturaRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Candidatura non trovata"));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Utente utente = (Utente) auth.getPrincipal();
+        if (!Objects.equals(candidatura.getMissione().getCreatore().getId(), utente.getId())) {
+            return false;
+        }
+        candidatura.setStato(Candidatura.CandidaturaStato.ACCETTATA);
+        return true;
+    }
+
+    public boolean rejectCandidatura(Long id) {
+        var candidatura = candidaturaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Candidatura non trovata"));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Utente utente = (Utente) auth.getPrincipal();
+        if (!Objects.equals(candidatura.getMissione().getCreatore().getId(), utente.getId())) {
+            return false;
+        }
+        candidatura.setStato(Candidatura.CandidaturaStato.RIFIUTATA);
+        return true;
+    }
+
+    @PreAuthorize("hasRole('VOLUNTEER')")
+    public Page<Candidatura> getCandidatureVolontario(Integer page, Integer pageSize) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Volontario volontario = (Volontario) auth.getPrincipal();
+        var pageable = Pageable.ofSize(pageSize).withPage(page);
+        return candidaturaRepository.findByCandidato(volontario, pageable);
+    }
+
+    public Page<Candidatura> getEsperienzeVolontario(Integer page, Integer pageSize) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Utente utente = (Utente) auth.getPrincipal();
+        var pageable = Pageable.ofSize(pageSize).withPage(page);
+        return candidaturaRepository.findByCandidatoAndStatoAndMissioneInternalStatoAndMissioneDataFineBefore(
+                utente,
+                Candidatura.CandidaturaStato.ACCETTATA,
+                Missione.InternalMissioneStato.ACCETTATA,
+                LocalDate.now(),
+                pageable);
+
+    }
+
+    public Page<Candidatura> getRichiesteCandidatura(Integer page, Integer pageSize) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Utente utente = (Utente) auth.getPrincipal();
+        var pageable = Pageable.ofSize(pageSize).withPage(page);
+        return candidaturaRepository.findByOrganizzatore(utente, pageable);
+    }
+
 }
