b4d30390e86e43bdaf71dc3c64ef8fa6a4b135da
f-niemiec <f.niemiec@studenti.unisa.it>
2026-01-18 14:39:42 +0100

    feat: restructured tests and started working on GestioneMissione

---
+++ Deliverables/Code/src/test/java/com/earthlocals/earthlocals/integration/gestionemissione/GestioneMissioneBottomUpIntegrationTest.java
@@ -0,0 +1,91 @@
+package com.earthlocals.earthlocals.integration.gestionemissione;
+
+import com.earthlocals.earthlocals.config.SystemTestAppConfig;
+import com.earthlocals.earthlocals.config.TestcontainerConfig;
+import com.earthlocals.earthlocals.model.*;
+import com.earthlocals.earthlocals.service.gestionemissioni.GestioneMissione;
+import com.earthlocals.earthlocals.service.gestionemissioni.dto.MissioneDTO;
+import com.earthlocals.earthlocals.service.gestionemissioni.pictures.PicturesFilesystemStorage;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@ContextConfiguration(classes = {SystemTestAppConfig.class, TestcontainerConfig.class})
+@Testcontainers
+@Transactional
+public class GestioneMissioneBottomUpIntegrationTest {
+    private final Resource pictureResource = new ClassPathResource("static/resources/files/sample.png");
+    @MockitoSpyBean
+    private Validator validator;
+    @MockitoSpyBean
+    private MissioneRepository missioneRepository;
+    @MockitoSpyBean
+    private PicturesFilesystemStorage storageService;
+    @MockitoSpyBean
+    private PaeseRepository paeseRepository;
+    @MockitoSpyBean
+    private UtenteRepository utenteRepository;
+    @MockitoSpyBean
+    private GestioneMissione gestioneMissione;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    private MissioneDTO validMissioneDTO() throws Exception{
+        var picture = new MockMultipartFile("sample.png", pictureResource.getInputStream());
+        var nazioneId = 1;
+
+        MissioneDTO missioneDTO = new MissioneDTO();
+        missioneDTO.setCreatore(utenteRepository.findAll().iterator().next());
+        missioneDTO.setDataInizio(LocalDate.now().plusDays(2));
+        missioneDTO.setDataFine(LocalDate.now().plusDays(3));
+        missioneDTO.setNome("Help teaching a Pechino ");
+        missioneDTO.setPaese(nazioneId);
+        missioneDTO.setCitta("Beijing");
+        missioneDTO.setFoto(picture);
+        missioneDTO.setDescrizione("Descrizione di almeno 20 caratteri");
+        missioneDTO.setCompetenzeRichieste("Competenze richieste");
+        missioneDTO.setRequisitiExtra("Requisiti extra");
+        return missioneDTO;
+    }
+
+
+    @Test
+    @WithMockUser(roles = "ORGANIZER")
+    void registerMissione() throws Exception {
+        var missioneDTO = validMissioneDTO();
+
+        var res = assertDoesNotThrow(() -> gestioneMissione.registerMissione(missioneDTO));
+        var missioneCaptor = ArgumentCaptor.forClass(Missione.class);
+        verify(missioneRepository, times(1)).save(missioneCaptor.capture());
+        var savedMissione = missioneCaptor.getValue();
+        assertSame(savedMissione, res);
+    }
+}
